cd05334bf6482693d244cefeeaf77feff7401d76
unknown <jee77562@udelasalle.edu.mx>
2024-08-19 09:15:41 -0600

    practica1

---
+++ practica1/js/hola.js
@@ -0,0 +1,5 @@
+var mensaje = "Hola TS";
+console.log(mensaje);
+var numero = 10;
+numero = 3.1416;
+console.log(numero);
---
+++ practica1/src/hola.ts
@@ -0,0 +1,6 @@
+let mensaje:string = "Hola TS";
+console.log(mensaje);
+
+let numero:number =10;
+numero =3.1416;
+console.log(numero);
